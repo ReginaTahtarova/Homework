c77139627b9e5d08125030283a6dce54c665532f
ReginaTahtarova <regi.rr@mail.ru>
2016-05-07 21:58:16 +0300

    lab6MVC
    lab6MVC

---
+++ lab1/lab1/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using lab1.Models;
+
+namespace lab1.Controllers
+{
+    public class HomeController : Controller
+    {
+        Data dt = new Data();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public void CreateNotepad(string NoteName)
+        {
+           dt.AddNotepad(NoteName);
+        }
+
+        [HttpPost]
+        public JsonResult LoadNotepads()
+        {  
+            return Json(dt.LoadNotes(), JsonRequestBehavior.AllowGet);
+        }
+
+        public string LoadContent(string NoteName)
+        {
+            return dt.GetContent(NoteName);
+        }
+
+        public void SetContent(string NoteName, string NoteContent)
+        {
+            dt.SetContent(NoteName, NoteContent);
+        }
+
+        public void CreateImage(string notepad)
+        {
+            dt.CreateImage(notepad);
+        }
+
+    }
+}
